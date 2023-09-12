e6f9f1c0356f629d57d2b912549a4bcac23194c2
1shyamp9 <1shyamp9@gmail.com>
2023-09-12 19:35:13 +0530

    Third commit

--- utils/feture.js
+++ utils/feture.js
@@ -4,10 +4,10 @@ export const createCookie = async(user,res,message) =>{
     try {
         const token = await jwt.sign({_id:user.id},process.env.JWT_SECRATE);
         res.status(200).cookie('token',token,{
-            maxAge:15*60*1000,
-            httpOnly:true,
-            sameSite: process.env.NODE_ENV === "Development" ? 'lax' : 'none',
-            secure: process.env.NODE_ENV === "Development" ? false : true
+            httpOnly: true,
+            maxAge: 20 * 60 * 1000,
+            sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
+            secure: process.env.NODE_ENV === "Development" ? false : true,
         }).json({
             success:true,
             message
